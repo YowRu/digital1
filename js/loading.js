827fa839a70dcc18629bc9d72a6682a0f45b8664
AngelaYu <love_3221213@hotmail.com>
2019-02-20 12:15:15 +0800

    try

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("header").load("header.html");
+    $("ul.nav").load("nav.html");
+    $(".nav_service").load("nav_service.html");
+    $(".redNav").load("redNav.html");
+    $("footer").load("footer.html");
+});
