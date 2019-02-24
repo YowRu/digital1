f3f225cba9fc795932fdaf9ec9ad5614b1687641
YowRu <32123611+YowRu@users.noreply.github.com>
2019-02-24 11:14:46 +0800

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
     $("header").load("header.html");
-    $("ul.nav").load("nav.html");
-    $(".nav_service").load("nav_service.html");
-    $(".redNav").load("redNav.html");
     $("footer").load("footer.html");
 });
